063983d55d87486c55c7ba1e781345a9d33ae1a3
draco1513 <51796955+draco1513@users.noreply.github.com>
2021-09-15 02:14:19 -0500

    Solucion Temporal

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-import math from "mathjs";
+//import math from "mathjs";
 function ejercicio(n){    
     const x=(3+(Math.sqrt(5)));
     y=x**n;
     z=parseInt(y); 
     a=z.toString();
     i=a.length;
-    console.log(i);
+    //console.log(i);
     if (i>3){
         b=a.substr(-3)  
     } else {
         if (i==1){
-            console.log(i)
-            console.log(a)
+            //console.log(i)
+            //console.log(a)
             b='00'+a;
         }else{
             if(i==2){
-                console.log(i)
-                console.log(a)
+                //console.log(i)
+                //console.log(a)
                 b='0'+a;
             }
         }
     }
     return b;
 }
-console.log(ejercicio(2));
+console.log(ejercicio(1));
